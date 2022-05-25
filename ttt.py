a6f0299c958eb18a8413e197043c17aefdaf27d2
Ashutosh <83763944+mraj-556@users.noreply.github.com>
2022-05-26 01:00:39 +0530

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -1,11 +1,19 @@
 import cv2
 from tkinter import *
+import threading
+import time
 
 
-                        #################  Map  initialization #################
+                        #################  Map and variables  initialization #################
 
 map =[ [-1 for i in range(3)] for j in range(3) ]  # (3 X 3)  matrix
 btn_list = {}
+
+global player_turn,game_over
+player_turn = 1
+game_over = False
+
+
 
                     #################  Player &  Button  Class  Declaration #################
 
@@ -20,18 +28,30 @@ class player:
     def reset(self,loc_x,loc_y):
         map[loc_x][loc_y] = -1
 
+
 class Buttons:
     def __init__(self,b_name,b_pos):
         self.b_name = b_name
         self.b_size = [5,9]
         self.b_pos = b_pos
         self.button = Button(screen,text=' ',bg='blue',height=self.b_size[0],width = self.b_size[1] )
+        self.button.config( command=lambda:set_colour(self.button) )
         self.button.place( x=b_pos[0] , y=b_pos[1] )
 
-    def set_colour(self,clr):
-        self.button.config(bg=clr)
     def get_loc(self):
         print('x : ',self.b_pos[0],'y : ',self.b_pos[1])
+
+
+                        ##################  button  clicked  ##################
+
+def set_colour(button):
+        # if player_turn == 1:
+        #     clr = 'green'
+        # elif player_turn ==2:
+        #     clr = 'red'
+        clr = 'green'
+        button.config(bg=clr)
+
 
                         #################  Player  Object  initialization #################
 
@@ -42,17 +62,22 @@ p_2 = player('Player 2',0)
 
 def multiplayer():
     p1_name = input('Player 1 : ')
-    p1_type = input('Type (x / o) : ').lower()
+    p2_type = 0
+    
+    while True:
+        p1_type = input('Type (x / o) : ').lower()
+        if p1_type == 'x':
+            p1_type = 'X'
+            p2_type = 'O'
+            break
+        elif p1_type == 'o':
+            p1_type = 'O'
+            p2_type = 'X'
+            break
+        else:
+            print('Invalid Choice')
 
     p2_name = input('Player 2 : ')
-    p2_type = 0
-
-    if p1_type == 'x':
-        p1_type = 'X'
-        p2_type = 'O'
-    elif p1_type == 'o':
-        p1_type = 'O'
-        p2_type = 'X'
 
     p_1.p_name = p1_name
     p_1.p_type = p1_type
@@ -64,15 +89,19 @@ def multiplayer():
 
 def bot():
     p_name = input('Name : ')
-    p_type = input('Type (x / o) : ').lower()
     bot_type = 0
-
-    if p_type == 'x':
-        p_type = 'X'
-        bot_type = 'O'
-    elif p_type == 'o':
-        p_type = 'O'
-        bot_type = 'X'
+    while True:
+        p_type = input('Type (x / o) : ').lower()
+        if p_type == 'x':
+            p_type = 'X'
+            bot_type = 'O'
+            break
+        elif p_type == 'o':
+            p_type = 'O'
+            bot_type = 'X'
+            break
+        else:
+            print('Invalid choice')
     
     p_1.p_name = p_name
     p_1.p_type = p_type
@@ -109,7 +138,7 @@ def create_button():
 
                         #################  mode selection and details input #################
 
-def game_start():
+def game_setup():
     print('Modes : ')
     print('     1. Two player2.')
     print('     2. Bot.')
@@ -130,13 +159,33 @@ def game_start():
         except:
             print('Invalid Selection...')
 
+                    ########################  game start  ########################
+
+def game_start():
+    global player_turn,game_over
+    while not game_over:
+        if player_turn == 1:
+            print('player_1')
+            player_turn = 2
+        elif player_turn == 2:
+            print('player_2')
+            player_turn = 1
+        time.sleep(1)
+
+def create_thread():
+    t = threading.Thread(target=game_start)
+    t.daemon = True
+    t.start()
+
             ##################################  main  code #####################################
 
-game_start()
+game_setup()
 
 screen = Tk()
 screen.geometry('700x500+340+100')
 screen.title('... Tik -- Tak -- Toe ...')
+
 create_button()
+create_thread()
 
 screen.mainloop()
